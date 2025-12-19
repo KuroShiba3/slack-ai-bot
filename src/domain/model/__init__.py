67c124e96befe7416741cf9b227a97bdd3ab3ff3
suguru shiroishi <s.shiroishi@branu.jp>
2025-12-19 19:37:48 +0900

    AgentName, TaskStatusをimport

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -3,7 +3,7 @@ from .feedback import Feedback
 from .final_answer import FinalAnswer
 from .general_answer_task_log import GeneralAnswerTaskLog
 from .message import Message
-from .task import Task
+from .task import Task, AgentName, TaskStatus
 from .task_log import TaskLog
 from .task_plan import TaskPlan
 from .web_search_task_log import WebSearchTaskLog
