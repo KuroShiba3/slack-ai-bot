8fc1cfc8a5614fd9100bf531442cbc80a8d75bab
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-20 18:54:51 +0900

    FinalAnswerを削除

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -1,9 +1,9 @@
 from .conversation import Conversation
 from .feedback import Feedback
-from .final_answer import FinalAnswer
 from .general_answer_task_log import GeneralAnswerTaskLog
-from .message import Message
+from .message import Message, Role
 from .task import Task, AgentName, TaskStatus
 from .task_log import TaskLog
 from .task_plan import TaskPlan
 from .web_search_task_log import WebSearchTaskLog
+from .workflow_result import WorkflowResult
