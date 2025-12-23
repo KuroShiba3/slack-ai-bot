f1f5ed365f03b8550f42180b1c646bc26afbb0fa
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-23 22:56:32 +0900

    TaskEvaluationをimport

--- src/domain/model/__init__.py
+++ src/domain/model/__init__.py
@@ -3,6 +3,7 @@ from .feedback import Feedback
 from .general_answer_task_log import GeneralAnswerTaskLog
 from .message import Message, Role
 from .task import Task, AgentName, TaskStatus
+from .task_evaluation import TaskEvaluation
 from .task_log import TaskLog
 from .task_plan import TaskPlan
 from .web_search_task_log import WebSearchTaskLog, SearchResult
