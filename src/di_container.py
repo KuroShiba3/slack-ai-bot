a98ddc63bfd896d92d27f2061f53232801436ffb
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-23 22:55:10 +0900

    ModelFactory/SlackMessageServiceを追加

--- src/di_container.py
+++ src/di_container.py
@@ -4,8 +4,8 @@ from slack_sdk.web.async_client import AsyncWebClient
 from .application.usecase.answer_to_user_request_usecase import AnswerToUserRequestUseCase
 from .infrastructure.repository.in_memory_chat_session_repository import InMemoryChatSessionRepository
 from .infrastructure.langgraph.graph.workflow_service import LangGraphWorkflowService
-from .infrastructure.llm import ModelFactory
-from .infrastructure.slack import SlackMessageService
+from .infrastructure.external.llm import ModelFactory
+from .infrastructure.external.slack import SlackMessageService
 from .presentation.controllers.slack_message_controller import SlackMessageController
 from .presentation.mapper.slack_request_mapper import SlackRequestMapper
 from .config import GOOGLE_API_KEY
@@ -23,7 +23,6 @@ class DIContainer:
         # ドメイン層
         self._workflow_service = LangGraphWorkflowService(
             model_factory=self._model_factory,
-            slack_service=self._slack_service,
             model_name="gemini-2.0-flash"
         )
 
