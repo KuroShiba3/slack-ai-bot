5b342999d5631e2bd48103cf6a5abfedf54bbc8f
suguru shiroishi <s.shiroishi@branu.jp>
2025-12-25 13:18:53 +0900

    ChatSessionRepositoryをprotocalに修正

--- src/domain/repository/chat_session_repository.py
+++ src/domain/repository/chat_session_repository.py
@@ -1,17 +1,13 @@
-from abc import ABC, abstractmethod
+from typing import Protocol
 
 from ..model.chat_session import ChatSession
 
 
-class IChatSessionRepository(ABC):
-    """チャットセッションの永続化を管理するリポジトリのインターフェース"""
-
-    @abstractmethod
+class ChatSessionRepository(Protocol):
     async def save(self, chat_session: ChatSession) -> None:
         """チャットセッションを保存"""
-        pass
+        ...
 
-    @abstractmethod
     async def find_by_id(self, chat_session_id: str) -> ChatSession | None:
         """IDでチャットセッションを取得"""
-        pass
+        ...
