c6c52e51843b9a04866c106d1cb84e05c061f32f
suguru shiroishi <s.shiroishi@branu.jp>
2025-12-25 13:19:52 +0900

    TaskLogに辞書形式に変換処理追加

--- src/domain/model/task_log.py
+++ src/domain/model/task_log.py
@@ -5,3 +5,12 @@ class TaskLog(Protocol):
     def add_attempt(self, **kwargs: Any) -> None:
         """試行をログに追加"""
         ...
+
+    def to_dict(self) -> dict[str, Any]:
+        """辞書形式に変換"""
+        ...
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "TaskLog":
+        """辞書形式から復元"""
+        ...
