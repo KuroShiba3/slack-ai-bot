b1dd5c4a2876dd466580feb8950c9478551ee14e
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-30 22:33:26 +0900

    event_id/message_tsの型修正

--- src/presentation/dto/slack_request_dto.py
+++ src/presentation/dto/slack_request_dto.py
@@ -6,8 +6,8 @@ class SlackRequestDTO:
     text: str
     user_id: str
     channel_id: str
+    event_id: str
+    message_ts: str
     thread_ts: str | None = None
-    message_ts: str | None = None
     team_id: str | None = None
-    event_id: str | None = None
     bot_id: str | None = None
