21987437dede275e9a01ba8012f9afafd9330209
s-shiroishi <suguru.shiroishi@gmail.com>
2025-12-31 16:25:16 +0900

    local.tf削除

--- terraform/environment/dev/local.tf
+++
@@ -1,7 +0,0 @@
-locals {
-    project_id            = var.project_id
-    region               = var.region
-    environment          = var.environment
-    service_name         = var.service_name
-    service_account_email = var.service_account_email
-}
